8f5aac7be31b7a52c74e808f20d5c72e7eb5d752
Hugo <hugo.hornquist@gmail.com>
2016-09-01 00:31:56 +0200

    changed up table definitions a bit

--- README.md
+++ README.md
@@ -3,3 +3,9 @@ A system for mapping keywords from documents into a database.
 
 Basicly hashtags. This goes along with my LaTeX class *note* which should be 
 available [here](https://github.com/hugonikanor/latexclasses").
+
+---
+
+This system currently assumes that no keyword appears multiple times in the same
+text...
+This might become a problem further down the line
--- create_tables.sql
+++ create_tables.sql
@@ -1,21 +1,20 @@
 CREATE TABLE keywords
 (
 	id INT NOT NULL AUTO_INCREMENT,
-	-- possibly make the name unique
 	name varchar(128) NOT NULL,
 
+	UNIQUE (name),
 	PRIMARY KEY (id)
 );
 
 CREATE TABLE courses
 (
 	id INT NOT NULL AUTO_INCREMENT,
-	-- possibly make the course unique
 	name VARCHAR(16) NOT NULL,
 
-	-- maybe add institution, since that would allow for url's to be contructed
-	--  from the data, otherwise just the url to the course could be added 
+	department VARCHAR(16) NOT NULL,
 
+	UNIQUE (name),
 	PRIMARY KEY (id)
 );
 
@@ -23,12 +22,8 @@ CREATE TABLE notes
 (
 	id INT NOT NULL AUTO_INCREMENT,
 	name VARCHAR(128),
-	date varchar(16),
+	date varchar(16) NOT NULL,
 	course_id INT NOT NULL,
-
-	-- Maybe have an optional url or sub-url for material relevant to this note,
-	-- probably constructed by taking the base url from the courses table and 
-	-- concatenating the sub-url from this table
 
 	PRIMARY KEY (id)
 );
