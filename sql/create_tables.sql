f23c4ac33200ae91b86c6022f15bb13cc4b9ae18
Hugo <hugo.hornquist@gmail.com>
2016-09-01 20:45:52 +0200

    added sql comment about group_id on keywords

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,3 +1,8 @@
+-- group_id is used as a unique identifier when adding records.
+-- It allows me to add a mix of good and bad (as in duplicate)
+-- records and then retrive them all
+-- It's a LONG becouse I used the current time in ms for it, this
+-- should possibly be changed...
 CREATE TABLE keywords
 (
 	id INT NOT NULL AUTO_INCREMENT,
