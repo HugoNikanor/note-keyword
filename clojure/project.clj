3eb25f037ec6503d68d4af2278b33cc94b28e6ac
Hugo <hugo.hornquist@gmail.com>
2016-09-03 12:59:09 +0200

    added command line parsing for database name and the like in clojure

--- clojure/project.clj
+++ clojure/project.clj
@@ -1,5 +1,6 @@
 (defproject sql-wrapper "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.8.0"]
+  				 [org.clojure/tools.cli "0.3.5"]
                  [org.clojure/java.jdbc "0.6.1"]
                  [mysql/mysql-connector-java "5.1.25"]]
   :aot [sql-wrapper.core]
--- clojure/src/sql_wrapper/core.clj
+++ clojure/src/sql_wrapper/core.clj
@@ -1,15 +1,30 @@
 (ns sql-wrapper.core
-  (:require [sql-wrapper.insert :as db])
+  (:require [sql-wrapper.insert :as db]
+            [clojure.tools.cli :refer [parse-opts]])
   (:gen-class))
 
-;; MAIN
+
 (defn -main [& args]
-  (def reader (new java.io.BufferedReader *in*))
-  (let [file-name (.readLine reader)
-        course (.readLine reader)
-        name (.readLine reader)
-        keywords (line-seq reader)]
-    (db/add-keywords-main! :date file-name
-                           :course course
-                           :document_name name
-                           :keywords keywords)))
+
+  (def option-vector
+    [["-d" "--database DATABASE" "MySQL database name"
+      :default "note_keywords"]
+     ["-u" "--user USER" "MySQL user"
+      :default "root"]
+     ["-p" "--password PASSWORD" "MySQL password for USER"
+      :default ""]
+     ["-h" "--help"]])
+
+  (let [{:keys [options arguments errors summary]} (parse-opts args option-vector)]
+    (if (:help options)
+      (do (println summary)
+          (System/exit 0))
+      (let [reader (new java.io.BufferedReader *in*)
+            file-name (.readLine reader)
+            course (.readLine reader)
+            name (.readLine reader)
+            keywords (line-seq reader)]
+        (db/add-keywords-main! :date file-name
+                               :course course
+                               :document_name name
+                               :keywords keywords)))))
