cd65f7fd0e6cd4aefc0992a20ec950dd6d81fe7b
Hugo <hugo.hornquist@gmail.com>
2016-09-05 00:11:27 +0200

    updated lein project.clj to release

--- clojure/project.clj
+++ clojure/project.clj
@@ -1,4 +1,7 @@
-(defproject sql-wrapper "0.1.0-SNAPSHOT"
+(defproject sql-wrapper "1.0.0-RELEASE"
+  :description "A small program which takes data and writes it to a
+               database. Should probably be rewritten in something
+               which doesn't require a JVM to start"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/tools.cli "0.3.5"]
                  [org.clojure/java.jdbc "0.6.1"]
